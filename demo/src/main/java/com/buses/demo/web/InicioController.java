8080958b49fec9a74bde33df23f0615a7e6a92b9
edwinpaye <baratheonts@gmail.com>
2019-11-14 20:10:50 -0400

    add @CrossOrigin in InicioController class

--- demo/src/main/java/com/buses/demo/web/InicioController.java
+++ demo/src/main/java/com/buses/demo/web/InicioController.java
@@ -1,10 +1,12 @@
 package com.buses.demo.web;
 
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 @Api(value="onlinestore", description="Operations")
 public class InicioController {
