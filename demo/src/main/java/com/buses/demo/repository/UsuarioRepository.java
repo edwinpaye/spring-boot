c7590266a53d34187620b5690cad79653af53120
edwinpaye <baratheonts@gmail.com>
2019-11-18 16:06:12 -0400

    add findByNombreContainingIgnoreCase method in UsuarioRepository interface

--- demo/src/main/java/com/buses/demo/repository/UsuarioRepository.java
+++ demo/src/main/java/com/buses/demo/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package com.buses.demo.repository;
 import com.buses.demo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    List<Usuario> findByNombreContainingIgnoreCase(String title);
 }
