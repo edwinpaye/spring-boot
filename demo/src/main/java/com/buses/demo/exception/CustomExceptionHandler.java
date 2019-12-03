d72f311edd4df0ec3e0318d14519c30cccaf571e
edwinpaye <baratheonts@gmail.com>
2019-12-03 02:01:45 -0400

    add CustomExceptionHandler class

---
+++ demo/src/main/java/com/buses/demo/exception/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.buses.demo.exception;
+
+public class CustomExceptionHandler {
+
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public Map<String, String> handleValidationExceptions(
+//            MethodArgumentNotValidException ex) {
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult().getAllErrors().forEach((error) -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//        return errors;
+//    }
+}
