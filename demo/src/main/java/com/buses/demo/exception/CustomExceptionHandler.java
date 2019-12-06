e7ae6cbccc3d5e2564f9772865e5c9fad93815da
edwinpaye <baratheonts@gmail.com>
2019-12-05 20:17:46 -0400

    update handlerAllExceptions and handleRecordNotFoundExceptions methods

--- demo/src/main/java/com/buses/demo/exception/CustomExceptionHandler.java
+++ demo/src/main/java/com/buses/demo/exception/CustomExceptionHandler.java
@@ -4,33 +4,30 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentTypeMismatchException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-//SuppressWarnings({"unchecked","rawtypes"})
+//@SuppressWarnings({"unchecked","rawtypes"})
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex/*, WebRequest request*/) {
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
+        List<String> details = Stream.of(ex.getLocalizedMessage()).collect(Collectors.toList());
         ErrorResponse error = new ErrorResponse("Server Error", details);
         return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleRecordNotFoundExceptions(RecordNotFoundException ex) {
-        List<String> details = new ArrayList<>();
-        details.add(ex.getLocalizedMessage());
+        List<String> details = Stream.of(ex.getLocalizedMessage()).collect(Collectors.toList());
         ErrorResponse error = new ErrorResponse("Record Not Found", details);
         return new ResponseEntity(error, HttpStatus.NOT_FOUND);
     }
