b9bb880a21b44b68328baad1a07abfff063afb9c
edwinpaye <baratheonts@gmail.com>
2019-12-07 09:22:38 -0400

    update getPageAllUsuarios method

--- demo/src/main/java/com/buses/demo/web/UsuarioController.java
+++ demo/src/main/java/com/buses/demo/web/UsuarioController.java
@@ -8,16 +8,18 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -47,11 +49,18 @@ public class UsuarioController {
 
     @ApiOperation(value = "Page all Usuarios", response = Usuario.class)
     @RequestMapping(method = RequestMethod.GET, value = "/pages", produces = { "application/hal+json" })
-    public ResponseEntity<Page<Usuario>> getPageAllUsuarios(
-            @RequestParam(value = "page", required = false, defaultValue = "0") Integer pagina){
-        Page<Usuario> page = usuarioService.getAllUsuarios(
-            PageRequest.of(pagina, 2, Sort.by("nombre").descending()/*.and(Sort.by("name"))*/));
-        return ResponseEntity.ok(page);
+    public ResponseEntity<PagedResources<Usuario>> getPageAllUsuarios(
+//            @RequestParam(value = "page", required = false, defaultValue = "0") Integer pagina,
+            Pageable pageable,
+            PagedResourcesAssembler paged){
+        Page < Usuario > products = usuarioService.getAllUsuarios(pageable);
+        PagedResources< Usuario > pr = paged.toResource(products, linkTo(UsuarioController.class).slash("/products").withSelfRel());
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        responseHeaders.add("Link", createLinkHeader(pr));
+        return new ResponseEntity < > (paged.toResource(products, linkTo(UsuarioController.class).slash("/products").withSelfRel()), HttpStatus.OK);
+//        Page<Usuario> page = usuarioService.getAllUsuarios(
+//            PageRequest.of(pagina, 2, Sort.by("nombre").descending()/*.and(Sort.by("name"))*/));
+//        return ResponseEntity.ok(page);
     }
 
     @ApiOperation(value = "Search a User with an Id", response = Usuario.class)
@@ -86,8 +95,8 @@ public class UsuarioController {
                 linkTo(methodOn(UsuarioController.class).getUsuarioById(usuario.getId())).withSelfRel(),
                 linkTo(methodOn(UsuarioController.class).getAllUsuarios()).withRel("usuarios")))
             .collect(Collectors.collectingAndThen(Collectors.toList(),
-                    usuarios -> ResponseEntity.ok(new Resources<>(usuarios,
-                    linkTo(methodOn(UsuarioController.class).getUsuariosByName(name)).withSelfRel()))));
+                usuarios -> ResponseEntity.ok(new Resources<>(usuarios,
+                linkTo(methodOn(UsuarioController.class).getUsuariosByName(name)).withSelfRel()))));
     }
 
     @ApiOperation(value = "Add new Usuario", response = Usuario.class)
