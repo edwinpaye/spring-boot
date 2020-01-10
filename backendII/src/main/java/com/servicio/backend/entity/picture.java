fb0b5795fff2ced17e81cb2ca3b1f9eb798eaa1f
edwinpaye <baratheonts@gmail.com>
2020-01-10 00:16:07 -0400

    add properties in picture class

--- backendII/src/main/java/com/servicio/backend/entity/picture.java
+++ backendII/src/main/java/com/servicio/backend/entity/picture.java
@@ -1,4 +1,23 @@
 package com.servicio.backend.entity;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "pictures")
 public class picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String name;;
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date create;
 }
