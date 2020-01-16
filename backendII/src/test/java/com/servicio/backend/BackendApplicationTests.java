53b1bc60b4f49dfb29d34dd3d7494d5e2192e545
edwinpaye <baratheonts@gmail.com>
2020-01-16 01:08:13 -0400

    remove test

--- backendII/src/test/java/com/servicio/backend/BackendApplicationTests.java
+++ backendII/src/test/java/com/servicio/backend/BackendApplicationTests.java
@@ -1,8 +1,5 @@
 package com.servicio.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 class BackendApplicationTests {
 
