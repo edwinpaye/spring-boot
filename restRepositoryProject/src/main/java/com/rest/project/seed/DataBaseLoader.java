0e0bb95e16636df1438a12f924caffbc2ddeb18f
edwinpaye <baratheonts@gmail.com>
2020-06-02 20:05:37 -0400

    add DataBaseLoader class

--- restRepositoryProject/src/main/java/com/rest/project/seed/DataBaseLoader.java
+++ restRepositoryProject/src/main/java/com/rest/project/seed/DataBaseLoader.java
@@ -7,30 +7,32 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
-@Component
+@Configuration
 public class DataBaseLoader {
 
-//    private static final Logger log = LoggerFactory.getLogger(DataBaseLoader.class);
+    private static final Logger log = LoggerFactory.getLogger(DataBaseLoader.class);
 
-    @Value("${deploy.roles}")
+    @Value("${deploy.productos}")
     private boolean deploy;
     @Bean
-    CommandLineRunner initDataBase(ProductoRepository repository){
+    CommandLineRunner loadProductos(ProductoRepository repository){
         return args -> {
             if (deploy){
-                repository.save(
-                        new Producto("Galletas", 0.0f, new Date()));
-//                log.info("Preloading" + repository.save(
-//                        new Producto("Galletas", 12.50f, new Date(new Date().getTime()))));
-//                log.info("Preloading" + repository.save(
-//                        new Producto("Gelatina", 18.50f, new Date(new Date().getTime()))));
-//                log.info("Preloading" + repository.save(
-//                        new Producto("Helado", 50.50f, new SimpleDateFormat().parse("1/5/2021"))));
-                System.out.println("hola");
+                SimpleDateFormat sDF = new SimpleDateFormat("dd-mm-yyyy");
+                try{
+                    log.info("Preloading " + repository.save(
+                            new Producto("Jugo", 12.50f, sDF.parse("1-2-2021"))));
+                    log.info("Preloading " + repository.save(
+                            new Producto("Gelatina", 18.50f, sDF.parse("2-1-2021"))));
+                    log.info("Preloading " + repository.save(
+                            new Producto("Helado", 50.50f, sDF.parse("3-4-2021"))));
+                }catch (Exception e){
+                    log.error("Error en el metodo loadProductos. " + e.getMessage());
+                }
             }
         };
     }
