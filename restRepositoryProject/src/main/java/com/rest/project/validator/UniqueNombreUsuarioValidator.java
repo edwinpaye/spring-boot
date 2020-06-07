c7d2995a395146849935d5c62740719d5db2cdee
edwinpaye <baratheonts@gmail.com>
2020-06-06 21:33:20 -0400

    add UniqueNombreUsuarioValidator class

--- restRepositoryProject/src/main/java/com/rest/project/validator/UniqueNombreUsuarioValidator.java
+++ restRepositoryProject/src/main/java/com/rest/project/validator/UniqueNombreUsuarioValidator.java
@@ -1,4 +1,23 @@
 package com.rest.project.validator;
 
-public class UniqueNombreUsuarioValidator {
+import com.rest.project.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueNombreUsuarioValidator implements ConstraintValidator<UniqueNombreUsuario, String> {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Override
+    public void initialize(UniqueNombreUsuario constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return usuarioRepo.existByNombreUsuario(s);
+    }
 }
