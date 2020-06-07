42395803abb2d2577d892b12569b9e648305fea3
edwinpaye <baratheonts@gmail.com>
2020-06-06 21:24:38 -0400

    add handleValidationException method

--- restRepositoryProject/src/main/java/com/rest/project/exception/CustomExceptionHandler.java
+++ restRepositoryProject/src/main/java/com/rest/project/exception/CustomExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.rest.project.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -13,4 +19,14 @@ public class CustomExceptionHandler {
 //    public String fatalUnexpectedException(HttpServletRequest req, Exception e){
 //        return e.getMessage();
 //    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        });
+        return errors;
+    }
 }
