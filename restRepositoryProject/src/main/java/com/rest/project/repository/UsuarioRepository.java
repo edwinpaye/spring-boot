ee8a2406aa8965228f74accbafe1dc98a981faa2
edwinpaye <baratheonts@gmail.com>
2020-06-06 21:45:34 -0400

    add UniqueEmailValidator

--- restRepositoryProject/src/main/java/com/rest/project/repository/UsuarioRepository.java
+++ restRepositoryProject/src/main/java/com/rest/project/repository/UsuarioRepository.java
@@ -11,9 +11,12 @@ import java.util.Optional;
 @RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    @RestResource(path = "findByNombreUsuario", rel = "findByNombreUsuario")
-    public Optional<Usuario> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+//    @RestResource(path = "findByNombreUsuario", rel = "findByNombreUsuario")
+//    public Optional<Usuario> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 
     @RestResource(path = "existByNombreUsuario", rel = "existByNombreUsuario")
     public boolean existByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
+
+    @RestResource(path = "existByEmail", rel = "existByEmail")
+    public boolean existByEmail(@Param("email") String email);
 }
--- restRepositoryProject/src/main/java/com/rest/project/validator/UniqueEmailValidator.java
+++ restRepositoryProject/src/main/java/com/rest/project/validator/UniqueEmailValidator.java
@@ -1,5 +1,22 @@
 package com.rest.project.validator;
 
 
-public class UniqueEmailValidator {
+import com.rest.project.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Override
+    public void initialize(UniqueEmail constraintAnnotation) { }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return usuarioRepo.existByEmail(s);
+    }
 }
