e0630df4714a0f4d23c9297c7a3d1ef7f5b70ec5
edwinpaye <baratheonts@gmail.com>
2020-06-06 21:25:45 -0400

    add findByNombreUsuario method

--- restRepositoryProject/src/main/java/com/rest/project/repository/UsuarioRepository.java
+++ restRepositoryProject/src/main/java/com/rest/project/repository/UsuarioRepository.java
@@ -2,8 +2,15 @@ package com.rest.project.repository;
 
 import com.rest.project.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestController
+import java.util.Optional;
+
+@RepositoryRestResource(collectionResourceRel = "productos", path = "productos")
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @RestResource(path = "nombreUsuario", rel = "nombreUsuario")
+    public Optional<Usuario> findByNombreUsuario(@Param("nombreUsuario") String nombreUsuario);
 }
