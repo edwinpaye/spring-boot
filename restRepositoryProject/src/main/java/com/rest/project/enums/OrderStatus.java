d101966844536284ca47f88e3410ddf65605bc39
edwinpaye <baratheonts@gmail.com>
2020-06-20 20:59:35 -0400

    add valid method

--- restRepositoryProject/src/main/java/com/rest/project/enums/OrderStatus.java
+++ restRepositoryProject/src/main/java/com/rest/project/enums/OrderStatus.java
@@ -1,4 +1,16 @@
 package com.rest.project.enums;
 
 public enum OrderStatus {
+
+    BEING_CREATED, PAID_FOR, FULFILLED, CANCELED;
+
+    static boolean valid(OrderStatus currentStatus, OrderStatus newStatus) {
+        return switch(currentStatus){
+            case BEING_CREATED -> newStatus==PAID_FOR||newStatus==CANCELED;
+            case PAID_FOR -> newStatus==FULFILLED;
+            case FULFILLED -> false;
+            case CANCELED -> false;
+            default -> throw new RuntimeException("Unrecognized situation");
+        };
+    }
 }
