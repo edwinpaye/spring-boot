7fd8df10a9d649ce93886e4d83ea29552bae9b0b
edwinpaye <baratheonts@gmail.com>
2020-06-22 18:01:01 -0400

    implement RepresentationModelProcesor interface

--- restRepositoryProject/src/main/java/com/rest/project/processor/OrdenProcessor.java
+++ restRepositoryProject/src/main/java/com/rest/project/processor/OrdenProcessor.java
@@ -1,4 +1,15 @@
 package com.rest.project.processor;
 
-public class OrdenProcessor {
+import com.rest.project.entity.Orden;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrdenProcessor implements RepresentationModelProcessor<EntityModel<Orden>> {
+
+    @Override
+    public EntityModel<Orden> process(EntityModel<Orden> model) {
+        return null;
+    }
 }
