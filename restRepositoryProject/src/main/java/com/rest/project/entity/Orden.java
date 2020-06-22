baaa4b3c5a7bd18a013c3c9bcb8aa82610add46b
edwinpaye <baratheonts@gmail.com>
2020-06-21 21:23:18 -0400

    add OrdenRepository class

--- restRepositoryProject/src/main/java/com/rest/project/entity/Orden.java
+++ restRepositoryProject/src/main/java/com/rest/project/entity/Orden.java
@@ -1,4 +1,51 @@
 package com.rest.project.entity;
 
+import com.rest.project.enums.OrderStatus;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Orden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private OrderStatus orderStatus;
+    private String productos;
+
+    private Orden() {
+        this.orderStatus = OrderStatus.BEING_CREATED;
+    }
+
+    public Orden(String productos) {
+        this();
+        this.productos = productos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getProductos() {
+        return productos;
+    }
+
+    public void setProductos(String productos) {
+        this.productos = productos;
+    }
 }
--- restRepositoryProject/src/main/java/com/rest/project/enums/OrderStatus.java
+++ restRepositoryProject/src/main/java/com/rest/project/enums/OrderStatus.java
@@ -4,13 +4,20 @@ public enum OrderStatus {
 
     BEING_CREATED, PAID_FOR, FULFILLED, CANCELED;
 
-    static boolean valid(OrderStatus currentStatus, OrderStatus newStatus) {
-        return switch(currentStatus){
-            case BEING_CREATED -> newStatus==PAID_FOR||newStatus==CANCELED;
-            case PAID_FOR -> newStatus==FULFILLED;
-            case FULFILLED -> false;
-            case CANCELED -> false;
-            default -> throw new RuntimeException("Unrecognized situation");
-        };
+    static boolean valid(OrderStatus currentStatus, OrderStatus newStatus) throws RuntimeException{
+//        return switch(currentStatus){
+//            case BEING_CREATED -> newStatus==PAID_FOR||newStatus==CANCELED;
+//            case PAID_FOR -> newStatus==FULFILLED;
+//            case FULFILLED -> false;
+//            case CANCELED -> false;
+//            default -> throw new RuntimeException("Unrecognized situation");
+//        };
+        switch (currentStatus){
+            case BEING_CREATED: return newStatus==PAID_FOR||newStatus==CANCELED;
+            case PAID_FOR: return newStatus==FULFILLED;
+            case FULFILLED: return false;
+            case CANCELED: return false;
+            default: throw new RuntimeException("Unrecognized situation");
+        }
     }
 }
